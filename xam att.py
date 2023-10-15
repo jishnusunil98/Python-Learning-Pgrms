8da270ac796def01662f78ab4371d88d834f87d7
Jishnu Sunil <jishnutvm98@gmail.com>
2023-10-15 14:04:01 +0530

    update attance pgrm

--- xam att.py
+++ xam att.py
@@ -5,5 +5,7 @@ x=tol_std_attd/tol_at
 y=x*100
 if y>=75:
     print("{} is eligible for attend the exam".format(name))
+    print("{} has {} percentage of attance".format(name,y))
 else:
      print("{} is not eligible for attend the exam".format(name))
+     print("{} have only {} percentage of attance".format(name,y))
